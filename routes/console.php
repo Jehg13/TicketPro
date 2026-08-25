ca854123f488a8bf0c12d4f010b370116f497802
Jehg13 <jehg13072002@gmail.com>
2026-08-25 17:43:01 -0500

    Configuracion backups

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Schedule::call(function () {
-    app(\App\Http\Controllers\BackupsController::class)
-        ->ejecutarAutomatico();
-})->everyMinute();
